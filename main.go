aaef275c85e9ee8df76bc11670b34ea3d7d7d756
fizzday <fizzday@yeah.net>
2018-02-09 22:33:42 +0800

    document

--- main.go
+++ main.go
@@ -4,18 +4,10 @@ import (
 	"github.com/gohouse/kuaixinwen/router"
 	. "github.com/gohouse/kuaixinwen/bootstrap"
 	"fmt"
-	"net/http"
 )
 
 func main()  {
 	fmt.Println("start...,port:8888, visit: http://localhost:8888")
-
-	// 解决跨域问题
-	//ServeHTTP()
-	//var w http.ResponseWriter
-	//var r *http.Request
-	////LDNS(w, req)
-	//lvtao(w, r)
 
 	// 加载数据库
 	//defer DB.Close()
@@ -29,44 +21,4 @@ func main()  {
 	if err!=nil{
 		fmt.Println(err)
 	}
-}
-
-func StartServer() error {
-	return nil
-}
-func LDNS(w http.ResponseWriter, req *http.Request) {
-
-	if origin := req.Header.Get("Origin"); origin != "" {
-		w.Header().Set("Access-Control-Allow-Origin", origin)
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers",
-			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-	}
-	if req.Method == "OPTIONS" {
-		return
-	}
-	//// 响应http code
-	//w.WriteHeader(200)
-	//query := strings.Split(req.Host, ".")
-	//value, err := ldns.RAMDBMgr.Get(query[0])
-	//fmt.Println("Access-Control-Allow-Origin", "*")
-	//if err != nil {
-	//	io.WriteString(w, `{"message": ""}`)
-	//	return
-	//}
-	//
-	//io.WriteString(w, value)
-}
-
-func lvtao(w http.ResponseWriter, r *http.Request) {
-	//这儿可以加上一些判断,比如根据域名,而不是*
-	w.Header().Add("Access-Control-Allow-Origin", "*")
-	w.Header().Add("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-	w.Header().Add("Access-Control-Allow-Credentials", "true")
-
-	if r.Method == "OPTIONS" {
-		return
-	}
-	fmt.Fprintln(w, "hi,taotao")
 }
--- router/route.go
+++ router/route.go
@@ -1,44 +1,40 @@
 package router
 
 import (
+	// 引入控制器
 	"github.com/gohouse/kuaixinwen/controller"
+	// 引入dotweb
 	"github.com/devfeel/dotweb"
+	// 引入dotweb中间件
 	"github.com/devfeel/middleware/cors"
-	"github.com/gohouse/gorose/utils"
-	. "github.com/gohouse/kuaixinwen/bootstrap"
 )
 
-func getMore() {
-	DB.Table("users").First()
-}
-
 func Run(Route *dotweb.HttpServer) {
-	// 设置cors选项中间件
+	// 设置cors选项中间件, 并使用默认的跨域配置
 	option := cors.NewConfig().UseDefault()
 
-	// 首页测试
+	// 首页测试 /
 	Route.GET("/", func(ctx dotweb.Context) error {
 		return ctx.WriteString("快新闻首页!")
 	})
-	// json返回测试
+	// json返回测试 /json
 	Route.GET("/json", func(ctx dotweb.Context) error {
-		//for i := 0; i < 10; i++ {
-		//	go getMore()
-		//}
-		res, err := DB.Table("users").First()
-		if err != nil {
-			return ctx.WriteJson(utils.SuccessReturn(err))
-		}
-		return ctx.WriteJson(utils.SuccessReturn(res))
+		return ctx.WriteJson("浪里个浪...")
 	}).Use(cors.Middleware(option))
 
-	// 前台展示列表
+	// 前台展示列表 /getnewslist
 	Route.GET("/getnewslist", controller.GetNewsList).Use(cors.Middleware(option))
+	// 获取单条新闻信息 /getnewsbyid
+	Route.GET("/getnewsbyid", controller.GetNewsById)
 
 	// 后台管理
 	admin := Route.Group("/admin").Use(cors.Middleware(option))
+	// 新闻列表 /admin/newsdel
 	admin.GET("/getnewslist", controller.GetNewsList)
+	// 获取单条新闻信息 /admin/getnewsbyid
 	admin.GET("/getnewsbyid", controller.GetNewsById)
+	// 新闻增加或修改, 根据是否传了id来判定, 传了id就修改, 否则增加 /admin/newsaddoredit
 	admin.POST("/newsaddoredit", controller.NewsAddOrEdit)
+	// 新闻删除 /admin/newsdel
 	admin.POST("/newsdel", controller.NewsDel)
 }
