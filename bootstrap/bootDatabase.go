620b3adf7ca2b1f642bad4c7048410d12565d837
fizzday <fizzday@yeah.net>
2018-03-21 13:40:15 +0800

    config

--- bootstrap/bootDatabase.go
+++ bootstrap/bootDatabase.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gohouse/gorose"
 	"github.com/gohouse/kuaixinwen/config"
 	_ "github.com/go-sql-driver/mysql"
+	"fmt"
 )
 
 var DB gorose.Connection
@@ -12,11 +13,14 @@ func init() {
 	var err error
 	// 加载database
 	DB,err = gorose.Open(config.DbConfig)
+
 	if err!=nil{
+		fmt.Println(err)
 		panic("数据库链接失败")
 	}
 	errs := DB.Ping()
 	if errs!=nil{
+		fmt.Println(err)
 		panic("数据库链接失败")
 	}
 }
